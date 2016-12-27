ab65d752544d406ed0a7e38071171aa692c2976f
Jonah C. Wilson <jwilson373@gatech.edu>
2016-12-26 22:17:55 -0500

    Changed some formatting on add student tab

--- src/Logger/StudentTab.java
+++ src/Logger/StudentTab.java
@@ -2,6 +2,7 @@ package Logger;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -13,11 +14,8 @@ import javafx.scene.layout.VBox;
 public class StudentTab extends Tab{
 
     // Student fields
-    private TextField studentID;
-    private TextField firstName;
-    private TextField lastName;
-    private TextField gradeLevel;
-    private TextField school;
+    private Label idLabel, firstLabel, lastLabel;
+    private TextField studentID, firstName, lastName, gradeLevel, school;
 
     private VBox vBox;
 
@@ -25,13 +23,28 @@ public class StudentTab extends Tab{
         this.setText("Add Student");
         // Overall Content VBox
         vBox = new VBox();
-        // Student ID HBox
-        HBox  sID = new HBox();
-        sID.setPadding(new Insets(10, 10, 10, 10));
-        studentID = new TextField("Enter Student ID here");
-        sID.getChildren().add(studentID);
 
-        vBox.getChildren().add(sID);
+        // Student ID HBox
+        HBox sID = new HBox(10);
+        sID.setPadding(new Insets(20, 10, 20, 10));
+        sID.setAlignment(Pos.CENTER_LEFT);
+        idLabel = new Label("Student ID:");
+        studentID = new TextField("Enter ID here");
+        sID.getChildren().addAll(idLabel, studentID);
+
+
+        // First and Last Name Hbox
+        HBox names = new HBox(150);
+        firstName = new TextField("Enter First Name");
+        lastName = new TextField("Enter Last Name");
+        //names.setPadding(new Insets(10, 10, 10, 10));
+        names.getChildren().addAll(firstName, lastName);
+        //firstName.setPadding(new Insets(0, 50, 0, 0));
+        names.setAlignment(Pos.CENTER);
+
+        // Add all to vBox
+
+        vBox.getChildren().addAll(sID, names);
 
         this.setContent(vBox);
     }
